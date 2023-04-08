0186a971c967ee1ad94684fa251febe20af70eb5
power-pole03 <powerpoleyang@gmail.com>
2023-04-08 17:20:59 -0400

    Updating main.py
    By updating job_data dictionary with collected data, appended the dictionary to result list

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term=python&button=&sort=any_time"
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = "python"
 
 response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = (soup.find_all('section', class_= "jobs"))
     for job_section in jobs:
@@ -17,8 +18,16 @@ else:
             anchors = post.find_all('a')
             anchor = anchors[1]
             link = anchor['href']
-            company, kind, region = anchor.find_all('span', class_ ="company")
+            company, work_time, region = anchor.find_all('span', class_ ="company")
             title = anchor.find('span', class_ = "title")
-            print(company, kind, region, title)
-            print("///////////////////////")
-            print("///////////////////////")
+            job_data = {
+                'link' : link,
+                'company' : company.string,
+                'work time' : work_time.string,
+                'region' : region.string,
+                'position' : title.string
+            }
+            results.append(job_data)
+    for result in results:
+        print(result)
+        print("////////////")
